ccf34ca8cefcce7e2c543e04562d30aa878898f9
George Treviranus <geowtrev@gmail.com>
2024-12-14 11:50:19 -0600

    chore: refine lg padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${publicSans.className} ${publicSans.variable} ${kronaOne.variable} fg-body min-h-screen bg-base-100 antialiased`}
       >
-        <div className="grid min-h-screen lg:px-20 lg:pt-12 xl:px-24 xl:pt-16">
+        <div className="grid min-h-screen lg:px-16 lg:pt-10 xl:px-24 xl:pt-16">
           <div className="relative bg-base-200 lg:rounded-t-3xl">
             <Header />
             <div className="p-8 md:p-12 lg:p-24">{children}</div>

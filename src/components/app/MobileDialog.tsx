a3f85453b85230624b391b6fd087149230ded62e
George Treviranus <geowtrev@gmail.com>
2024-12-16 10:55:10 -0600

    chore: clean up mobile dialog navigation container

--- src/components/app/MobileDialog.tsx
+++ src/components/app/MobileDialog.tsx
@@ -13,17 +13,18 @@ export const MobileDialog = () => (
       </Button>
     </Dialog.Trigger>
     <Dialog.Portal>
-      <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-black/50" />
-      <Dialog.Content className="data-[state=open]:animate-contentShow fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+      <Dialog.Overlay className="fixed inset-0 bg-black/50 data-[state=open]:animate-overlayShow" />
+      <Dialog.Content className="fixed left-1/2 top-1/2 h-[100vh] w-[100vw] -translate-x-1/2 -translate-y-1/2 bg-white/50 p-5 backdrop-blur-md focus:outline-none data-[state=open]:animate-contentShow dark:bg-slate-950/50">
         <Dialog.Title className="sr-only">Navigation Dialog</Dialog.Title>
 
         <Dialog.Close asChild>
-          <button
-            className="absolute right-2.5 top-2.5 inline-flex size-[25px] appearance-none items-center justify-center rounded-full text-slate-900 hover:bg-slate-900 focus:shadow-[0_0_0_2px] focus:shadow-slate-900 focus:outline-none"
+          <Button
+            className="absolute right-5 top-5 inline-flex text-purple-950 dark:text-purple-100"
+            variant="ghost"
             aria-label="Close"
           >
             <X size={24} />
-          </button>
+          </Button>
         </Dialog.Close>
       </Dialog.Content>
     </Dialog.Portal>

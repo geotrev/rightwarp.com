050a528254a8ea72ddbb2cd4775e9ed3f3314957
George Treviranus <geowtrev@gmail.com>
2024-12-22 15:11:53 -0600

    fix: bg color on narrow widths

--- src/app/ClientBodyWrapper.tsx
+++ src/app/ClientBodyWrapper.tsx
@@ -11,7 +11,7 @@ export const ClientBodyWrapper = ({ children }: { children: React.ReactNode }) =
     <body
       suppressHydrationWarning
       data-theme={theme}
-      className="fg-body min-h-screen bg-base-100 antialiased transition-colors"
+      className="fg-body min-h-screen bg-base-200 antialiased transition-colors lg:bg-base-100"
     >
       <div className="grid min-h-screen transition-[padding] ease-in-out lg:px-16 lg:pt-10 xl:px-24 xl:pt-16">
         <div className="relative bg-base-200 transition-[border-radius,background-color] ease-in-out lg:rounded-t-3xl">

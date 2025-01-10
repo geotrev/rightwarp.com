a818c9b58050992d5458d7b1b9ec3a33ad175390
George Treviranus <geowtrev@gmail.com>
2025-01-09 19:16:36 -0600

    fix: enable automatic page views in ph

--- src/components/context/PostHogProvider.tsx
+++ src/components/context/PostHogProvider.tsx
@@ -1,17 +1,15 @@
-// app/providers.js
 "use client"
 
 import posthog from "posthog-js"
-import { PostHogProvider } from "posthog-js/react"
+import { PostHogProvider as PHProvider } from "posthog-js/react"
+import { useEffect } from "react"
 
-// app/providers.js
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    })
+  }, [])
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-  })
-}
-export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+  return <PHProvider client={posthog}>{children}</PHProvider>
 }

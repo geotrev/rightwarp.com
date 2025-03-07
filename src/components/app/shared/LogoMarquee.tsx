b7ac26ae735315a1505208d02b499e9c5cb100e6
George Treviranus <geowtrev@gmail.com>
2025-03-06 18:44:14 -0600

    fix: permanent pause scroll button

--- src/components/app/shared/LogoMarquee.tsx
+++ src/components/app/shared/LogoMarquee.tsx
@@ -34,50 +34,50 @@ export const LogoMarquee = () => {
     <section
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className="relative flex h-12 max-w-full items-center overflow-x-hidden text-purple-950 opacity-75 md:gap-3 lg:gap-12 dark:text-purple-100"
+      className="flex flex-col gap-2"
     >
-      {/* animation start */}
-      <div
-        className="absolute left-0 top-0 flex w-full min-w-[max-content] animate-marquee-start items-center justify-around"
-        style={{ animationPlayState: isPaused ? "paused" : "running" }}
-      >
-        {Logos.map(({ Logo, name }) => (
-          <div
-            key={name}
-            className="mx-10 w-24 flex-shrink-0 sm:w-28 lg:mx-20 lg:w-32"
-          >
-            <Logo />
-          </div>
-        ))}
-      </div>
-      {/* animation continuation - without it, the animation leaves white space after the initial logos */}
-      <div
-        className="absolute left-[full] top-0 flex w-full min-w-[max-content] animate-marquee-end items-center justify-around"
-        style={{ animationPlayState: isPaused ? "paused" : "running" }}
-      >
-        {Logos.map(({ Logo, name }) => (
-          <div
-            key={name}
-            className="mx-10 w-24 flex-shrink-0 sm:w-28 lg:mx-20 lg:w-32"
-          >
-            <Logo />
-          </div>
-        ))}
+      <div className="relative flex h-12 max-w-full items-center overflow-x-hidden text-purple-950 opacity-75 md:gap-3 lg:gap-12 dark:text-purple-100">
+        {/* animation start */}
+        <div
+          className="absolute left-0 top-0 flex w-full min-w-[max-content] animate-marquee-start items-center justify-around"
+          style={{ animationPlayState: isPaused ? "paused" : "running" }}
+        >
+          {Logos.map(({ Logo, name }) => (
+            <div
+              key={name}
+              className="mx-10 w-24 flex-shrink-0 sm:w-28 lg:mx-20 lg:w-32"
+            >
+              <Logo />
+            </div>
+          ))}
+        </div>
+        {/* animation continuation - without it, the animation leaves white space after the initial logos */}
+        <div
+          className="absolute left-[full] top-0 flex w-full min-w-[max-content] animate-marquee-end items-center justify-around"
+          style={{ animationPlayState: isPaused ? "paused" : "running" }}
+        >
+          {Logos.map(({ Logo, name }) => (
+            <div
+              key={name}
+              className="mx-10 w-24 flex-shrink-0 sm:w-28 lg:mx-20 lg:w-32"
+            >
+              <Logo />
+            </div>
+          ))}
+        </div>
       </div>
       <button
         type="button"
-        className={cn(
-          "btn btn-secondary btn-sm absolute left-6 z-10 opacity-0 focus-within:opacity-100 md:left-12 lg:left-24",
-          {
-            "opacity-100": isHovered,
-          },
-        )}
+        className={cn("btn btn-ghost btn-xs me-auto ms-6 md:ms-12 lg:ms-24", {
+          "opacity-75": !isHovered,
+          "opacity-100": isHovered,
+        })}
+        onFocus={() => setIsHovered(true)}
+        onBlur={() => setIsHovered(false)}
         onClick={handlePauseClick}
       >
         {isPaused ? <Play size={16} /> : <Pause size={16} />}
-        <span className="sr-only">
-          {isPaused ? "Play animation" : "Pause animation"}
-        </span>
+        {isPaused ? "Play scroll" : "Pause scroll"}
       </button>
     </section>
   )

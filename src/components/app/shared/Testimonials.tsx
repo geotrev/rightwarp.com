614b271ab98b3b97e7a28451e25f38f2b5fdd4ea
George Treviranus <geowtrev@gmail.com>
2025-03-09 20:35:54 -0500

    fix: add pause/play button to testimonial carousel

--- src/components/app/shared/Testimonials.tsx
+++ src/components/app/shared/Testimonials.tsx
@@ -1,14 +1,22 @@
 import cn from "classnames"
-import { LucideIcon } from "lucide-react"
+import {
+  ChevronLeft,
+  ChevronRight,
+  LucideIcon,
+  Pause,
+  Play,
+} from "lucide-react"
 import { AnimatePresence } from "motion/react"
 import * as motion from "motion/react-client"
 import Image from "next/image"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { TinaMarkdown, TinaMarkdownContent } from "tinacms/dist/rich-text"
 
 import { Container } from "@/components/core"
 
 import { SectionHeading } from "./SectionHeading"
+
+const TRANSITION_DELAY = 15000
 
 interface Testimonial {
   image?: string
@@ -30,17 +38,14 @@ export interface TestimonialsProps {
 
 const variants = {
   enter: {
-    x: 100,
     opacity: 0,
   },
   center: {
     zIndex: 1,
-    x: 0,
     opacity: 1,
   },
   exit: {
     zIndex: 0,
-    x: -100,
     opacity: 0,
   },
 }
@@ -82,6 +87,7 @@ const Testimonial = (props: Testimonial) => {
     <motion.div
       className="absolute"
       variants={variants}
+      transition={{ duration: 0.25 }}
       initial="enter"
       animate="center"
       exit="exit"
@@ -98,14 +104,59 @@ export const Testimonials = ({
   testimonials,
 }: TestimonialsProps) => {
   const [page, setPage] = useState<number>(0)
+  const [isPaused, setIsPaused] = useState<boolean>(false)
+  const [isHovered, setIsHovered] = useState<boolean>(false)
+  const intervalRef = useRef<NodeJS.Timeout | null>(null)
+
+  const createInterval = useCallback(() => {
+    intervalRef.current = setInterval(() => {
+      setPage((page) => (page + 1) % testimonials!.length)
+    }, TRANSITION_DELAY)
+  }, [testimonials])
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setPage((page) => (page + 1) % testimonials!.length)
-    }, 10000)
+    if (isPaused) {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current)
+        intervalRef.current = null
+      }
+    } else {
+      if (!intervalRef.current) {
+        createInterval()
+      }
+    }
 
-    return () => clearInterval(interval)
-  }, [page, testimonials])
+    return () => {
+      if (!intervalRef.current) return
+
+      clearInterval(intervalRef.current)
+      intervalRef.current = null
+    }
+  }, [testimonials, createInterval, isPaused])
+
+  const resetInterval = useCallback(() => {
+    if (!intervalRef.current || isPaused) return
+
+    clearInterval(intervalRef.current)
+    createInterval()
+  }, [intervalRef, createInterval, isPaused])
+
+  const handleNextClick = useCallback(() => {
+    if (!testimonials?.length) return
+
+    setPage((page) => (page + 1) % testimonials!.length)
+    resetInterval()
+  }, [resetInterval, testimonials])
+
+  const handlePrevClick = useCallback(() => {
+    if (!testimonials?.length) return
+
+    setPage((page) => (page - 1 + testimonials!.length) % testimonials!.length)
+    resetInterval()
+  }, [resetInterval, testimonials])
+
+  const handleMouseEnter = () => setIsHovered(true)
+  const handleMouseLeave = () => setIsHovered(false)
 
   if (!testimonials?.length) return null
 
@@ -114,8 +165,13 @@ export const Testimonials = ({
       <SectionHeading icon={icon} subheading={subheading}>
         {heading}
       </SectionHeading>
-      <Container isConstrained outerStyles="lg:px-28">
-        <div className="relative">
+      <Container
+        isConstrained
+        outerStyles="lg:px-28"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="relative mb-6">
           <AnimatePresence>
             {testimonials[page] && (
               <Testimonial key={page} {...testimonials[page]} />
@@ -125,6 +181,36 @@ export const Testimonials = ({
               <TestimonialContent {...testimonials[page]} isHidden />
             )}
           </AnimatePresence>
+        </div>
+        <div className="mb-2 flex justify-center gap-4">
+          <button
+            onClick={handlePrevClick}
+            className="btn btn-circle btn-ghost"
+            aria-label="Previous testimonial"
+          >
+            <ChevronLeft size={24} />
+          </button>
+          <button
+            onClick={handleNextClick}
+            className="btn btn-circle btn-ghost"
+            aria-label="Next testimonial"
+          >
+            <ChevronRight size={24} />
+          </button>
+        </div>
+        <div className="flex">
+          <button
+            className={cn("btn btn-ghost btn-xs mx-auto", {
+              "opacity-75": !isHovered,
+              "opacity-100": isHovered,
+            })}
+            onClick={() => setIsPaused((paused) => !paused)}
+            onFocus={() => setIsHovered(true)}
+            onBlur={() => setIsHovered(false)}
+          >
+            {isPaused ? <Play size={16} /> : <Pause size={16} />}
+            {isPaused ? "Enable auto-transition" : "Pause auto-transition"}
+          </button>
         </div>
       </Container>
     </section>
